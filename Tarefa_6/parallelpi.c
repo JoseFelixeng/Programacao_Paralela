cef8fff93eff4c21f698d8d39efeb1d87bebe758
JoseFelix <jfelixnunes2k15@gmail.com>
2026-08-31 17:12:29 -0300

    fix: add new modification on parallel.c

--- Tarefa_6/parallelpi.c
+++ Tarefa_6/parallelpi.c
@@ -6,25 +6,33 @@
 int main(){
     long long N = 10000000;
     long long pontos_dentro = 0;
-    double x,y;
+    int offset = 1; 
+    long long ultimo_i = -1;
 
-    srand((unsigned int)time(NULL));
 
     double inicio = omp_get_wtime();
-   #pragma omp parallel 
+   #pragma omp parallel default(none) shared(N, pontos_dentro) firstprivate(offset) lastprivate(ultimo_i)
    {
+        unsigned int seed = omp_get_thread_num() + offset;
+        long long local_dento_c = 0;
+        double x,y;
+
+
         #pragma omp for   
         for(long long i = 0; i < N; i++){
         //Gear as coordenadas de x e y entre 0 e 1
-        x = (double)rand()/RAND_MAX;
-        y = (double)rand()/RAND_MAX;
+            x = (double)rand_r(&seed)/RAND_MAX;
+            y = (double)rand_r(&seed)/RAND_MAX;
 
-        if((x*x)+(y*y) <= 1.0){
-            #pragma omp critical
-            pontos_dentro++;
+            if((x*x)+(y*y) <= 1.0){
+                local_dento_c++;
+            }
+            ultimo_i = i;
         }
-    }
+        #pragma omp critical 
+        pontos_dentro += local_dento_c;
    }
+
     double fim = omp_get_wtime();
 
     double pi_estimado = 4.0 * ((double)pontos_dentro / N);
